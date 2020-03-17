8686071578d10cfccea4392b9cd4c4b248d5b97a
Dani <daniel@dengun.com>
2020-03-17 23:05:41 +0000

    added configs for Firebase Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,23 @@
+import 'dart:async';
+
 import 'package:covid19mobile/ui/app.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(CovidApp());
+
+void main() {
+
+  // Set `enableInDevMode` to true to see reports while in debug mode
+  // This is only to be used for confirming that reports are being
+  // submitted as expected. It is not intended to be used for everyday
+  // development.
+  Crashlytics.instance.enableInDevMode = true;
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+  runZoned<Future<void>>(() async {
+    runApp(CovidApp());
+  }, onError: Crashlytics.instance.recordError);
+
+}

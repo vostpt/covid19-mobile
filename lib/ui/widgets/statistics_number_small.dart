88cac321e8780da8a82b145b630465e93e6d91e0
CodeGlitch <CodeGlitch@pm.me>
2020-03-18 11:44:05 -0100

    19 statistics number small
    Removed unnecessary Comment
    Removed Colors

--- lib/ui/widgets/statistics_number_small.dart
+++ lib/ui/widgets/statistics_number_small.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-///Widget StatisticsNumberSmall
 class StatisticsNumberSmall extends StatelessWidget {
   ///number displayed
   final int value;
+
   ///text displayed
   final String text;
 
@@ -17,12 +17,11 @@ class StatisticsNumberSmall extends StatelessWidget {
       children: <Widget>[
         Text(
           value.toString(),
-          style: TextStyle(
-              fontWeight: FontWeight.bold, color: Colors.black, fontSize: 28),
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
         ),
         Text(
           text,
-          style: TextStyle(color: Colors.black, fontSize: 18),
+          style: TextStyle(fontSize: 18),
         )
       ],
     );

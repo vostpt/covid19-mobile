2cd82d028c3fa0b7625ed501e820a3d6b50df6d7
CodeGlitch <CodeGlitch@pm.me>
2020-03-18 13:51:07 -0100

    32 Card FAQ
    Widget Proposal for Card_FAQ

---
+++ lib/ui/widgets/card_faq.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CardFAQ extends StatelessWidget {
+  final String text;
+  final String targetRoute;
+
+  CardFAQ(this.text, this.targetRoute);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, targetRoute);
+      },
+      child: Card(
+        color: Colors.blue,
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  text,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18),
+                ),
+              ),
+              Icon(
+                Icons.arrow_forward,
+                size: 30,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

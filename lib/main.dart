19172726f74394b7dac0af400db83cc53837b462
Dani <daniel@dengun.com>
2020-03-18 15:56:34 +0000

    fix dependencies on firebase

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,33 @@ import 'package:flutter/material.dart';
 
 void main() {
 
-  // Set `enableInDevMode` to true to see reports while in debug mode
-  // This is only to be used for confirming that reports are being
-  // submitted as expected. It is not intended to be used for everyday
-  // development.
-  Crashlytics.instance.enableInDevMode = true;
+  var enableInDevMode = true;
 
-  // Pass all uncaught errors from the framework to Crashlytics.
+  /// Set `enableInDevMode` to true to see reports while in debug mode
+  /// This is only to be used for confirming that reports are being
+  /// submitted as expected. It is not intended to be used for everyday
+  /// development.
+  Crashlytics.instance.enableInDevMode = enableInDevMode;
+
+  /// Pass all uncaught errors from the framework to Crashlytics.
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runZoned<Future<void>>(() async {
+
+    /// Run main app
     runApp(CovidApp());
-  }, onError: Crashlytics.instance.recordError);
+
+    }, onError: (e, s) {
+
+    /// Register and sends error
+    Crashlytics.instance.recordError(e, s);
+
+    /// for debug:
+    if(enableInDevMode) {
+      logger.e('[Error]: ${e.toString()}');
+      logger.e('[Stacktrace]: ${s.toString()}');
+    }
+
+  });
 
 }

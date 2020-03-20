d5c1dc82e68382a2bb8511c41c33513f90cfed66
danidroid <danielfjorge@gmail.com>
2020-03-20 19:49:00 +0000

    updates

--- lib/services/local_service.dart
+++ lib/services/local_service.dart
@@ -1,12 +1,27 @@
+///     This program is free software: you can redistribute it and/or modify
+///    it under the terms of the GNU General Public License as published by
+///    the Free Software Foundation, either version 3 of the License, or
+///    (at your option) any later version.
+///
+///    This program is distributed in the hope that it will be useful,
+///    but WITHOUT ANY WARRANTY; without even the implied warranty of
+///    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///    GNU General Public License for more details.
+///
+///    You should have received a copy of the GNU General Public License
+///    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import 'package:covid19mobile/ui/app.dart';
 import 'package:hive/hive.dart';
 
 import 'local/local_data_source.dart';
 
-/// Main LocalService
+/// Main LocalStorageService
 ///
 /// This will create a singleton instance
+///
+/// To use add this to getRequest
+/// await LocalStorageService.cache.saveToCache(cacheKey, results);
 ///
 class LocalStorageService {
   static const String _tag = '.LocalStorageService';

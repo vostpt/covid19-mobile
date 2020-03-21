d45a321775e3d43deab8447c4b6c5cc10e8ab1b0
Gonçalo Palma <solid.goncalo@gmail.com>
2020-03-21 19:03:18 +0000

    Adds About Page

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -38,6 +38,96 @@ class S {
       args: [],
     );
   }
+
+  String get screenAboutTitle {
+    return Intl.message(
+      'Sobre',
+      name: 'screenAboutTitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  String get screenAboutContent1 {
+    return Intl.message(
+      'Esta App pretende ser um guia prático para apoiar cidadãos, famílias e empresas no combate aos efeitos causados pelo COVID-19. Aqui poderá encontrar toda a informação e conhecer os apoios a que tem direito.',
+      name: 'screenAboutContent1',
+      desc: '',
+      args: [],
+    );
+  }
+
+  String get screenAboutContent2 {
+    return Intl.message(
+      'Consulte as boas práticas e as recomendações das autoridades de Saúde, conheça os melhores conselhos para trabalhar a partir de casa e saiba como recorrer aos serviços públicos sem ter de se deslocar.',
+      name: 'screenAboutContent2',
+      desc: '',
+      args: [],
+    );
+  }
+
+  String get screenAboutContent3 {
+    return Intl.message(
+      'Poderá ainda ficar a conhecer as medidas excecionais adotadas pelo Governo em cada área governativa e acompanhar as evoluções do estado epidemiológico do País. Está também disponível para consulta a legislação especificamente aprovada, bem como as diferentes comunicações do Governo nesta matéria.',
+      name: 'screenAboutContent3',
+      desc: '',
+      args: [],
+    );
+  }
+
+  String get screenAboutContent4 {
+    return Intl.message(
+      'Além destas informações, pode aceder à lista completa dos contactos de emergência criados pelos diversos serviços públicos para que, de forma simples e eficiente, encontre a resposta que procura.',
+      name: 'screenAboutContent4',
+      desc: '',
+      args: [],
+    );
+  }
+
+  String get screenAboutContent5 {
+    return Intl.message(
+      'Juntos venceremos este vírus.',
+      name: 'screenAboutContent5',
+      desc: '',
+      args: [],
+    );
+  }
+
+  String get screenAboutHashtag {
+    return Intl.message(
+      '#EstamosOn',
+      name: 'screenAboutHashtag',
+      desc: '',
+      args: [],
+    );
+  }
+
+  String get screenAboutBuilt {
+    return Intl.message(
+      'Esta app foi construída com o apoio da VOST PT e Flutter Portugal',
+      name: 'screenAboutBuilt',
+      desc: '',
+      args: [],
+    );
+  }
+
+  String get screenAboutFooter1 {
+    return Intl.message(
+      '#COVID19PT - 2020 VOSTPT - Voluntários Digitais em Situações de Emergências para Portugal.',
+      name: 'screenAboutFooter1',
+      desc: '',
+      args: [],
+    );
+  }
+
+  String get screenAboutFooter2 {
+    return Intl.message(
+      'Este site e seus materiais encontram-se sob uma licença Creative Commons CC BY-NC-SA: Atribuição-Não Comercial-Compartilha Igual.',
+      name: 'screenAboutFooter2',
+      desc: '',
+      args: [],
+    );
+  }
 }
 
 class AppLocalizationDelegate extends LocalizationsDelegate<S> {

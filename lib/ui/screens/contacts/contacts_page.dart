1cd3ab857467f1b227411f5baa601d85353fb60d
danidroid <danielfjorge@gmail.com>
2020-03-22 23:53:49 +0000

    hotfix/contacts_link

--- lib/ui/screens/contacts/contacts_page.dart
+++ lib/ui/screens/contacts/contacts_page.dart
@@ -106,7 +106,13 @@ class _ContactsPageState extends State<ContactsPage> {
         _launch("tel: ${contact.title}");
         break;
       case ContactType.link:
-        _launch(contact.title);
+        var urlToOpen = contact.title;
+        if (!(contact.title.startsWith("https://") ||
+            contact.title.startsWith("http://"))) {
+          urlToOpen = "https://${contact.title}";
+        }
+
+        _launch(urlToOpen);
         break;
       case ContactType.email:
         _launch("mailto: ${contact.title}");

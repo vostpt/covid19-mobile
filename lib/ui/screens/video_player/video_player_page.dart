8f16b40b1b1b715a19a6ebdd896896769b4779a7
CodeGlitch <CodeGlitch@pm.me>
2020-03-23 16:37:23 -0100

    Added icon background
    Added icon semi transparent background
    Added License

--- lib/ui/screens/video_player/video_player_page.dart
+++ lib/ui/screens/video_player/video_player_page.dart
@@ -1,3 +1,16 @@
+///     This program is free software: you can redistribute it and/or modify
+///    it under the terms of the GNU General Public License as published by
+///    the Free Software Foundation, either version 3 of the License, or
+///    (at your option) any later version.
+///
+///    This program is distributed in the hope that it will be useful,
+///    but WITHOUT ANY WARRANTY; without even the implied warranty of
+///    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///    GNU General Public License for more details.
+///
+///    You should have received a copy of the GNU General Public License
+///    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import 'package:covid19mobile/ui/assets/dimensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -47,9 +60,15 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
               alignment: Alignment.topRight,
               child: Padding(
                 padding: const EdgeInsets.all(marginVidCloseBt),
-                child: IconButton(
-                  icon: Icon(Icons.close, color: Colors.white),
-                  onPressed: () => Navigator.of(context).pop(),
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black54,
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.close, color: Colors.white),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
                 ),
               ),
             ),

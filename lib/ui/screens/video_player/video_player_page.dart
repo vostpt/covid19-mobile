c2c574fe7b002ecc454b653728c8551c4f964f64
CodeGlitch <CodeGlitch@pm.me>
2020-03-23 15:41:44 -0100

    Moved code from Videos_Page to Video_Player_Page
    Code should be called on Video_Player_Page not Videos_Page like I was doing
    Changed close button from appBar to body

--- lib/ui/screens/video_player/video_player_page.dart
+++ lib/ui/screens/video_player/video_player_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoPlayerPage extends StatefulWidget {
@@ -14,6 +15,13 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.portraitDown,
+    ]);
+
     _url = ModalRoute.of(context).settings.arguments;
     _controller = YoutubePlayerController(
       initialVideoId: _url,
@@ -22,24 +30,26 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
       ),
     );
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        elevation: 0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.close, color: Colors.white),
-            onPressed: () => Navigator.of(context).pop(),
-          )
-        ],
-        leading: Container(),
-      ),
       backgroundColor: Colors.black,
-      body: Center(
-        child: YoutubePlayer(
-          controller: _controller,
-          onReady: () {
-            _controller.addListener(() {});
-          },
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: Icon(Icons.close, color: Colors.white),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ),
+            Center(
+              child: YoutubePlayer(
+                controller: _controller,
+                onReady: () {
+                  _controller.addListener(() {});
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -48,6 +58,9 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
   @override
   void dispose() {
     _controller.dispose();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     super.dispose();
   }
 }

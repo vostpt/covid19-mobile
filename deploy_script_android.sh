952351cf9e3e000ddc1d10db0899a8bcf8a57344
Miguel Ruivo <miguel@miguelruivo.com>
2020-04-03 22:33:17 +0100

    Updates Travis script to use environment variables only on deploy scripts

--- deploy_script_android.sh
+++ deploy_script_android.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -d -a -in ./android/key.properties.enc -out ./android/key.properties
+openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -d -a -in ./android/keystore.enc -d -a -out ./android/app/key.jks
+openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -d -a -in ./android/app/google-services.json.enc -d -out ./android/app/google-services.json
 curl -sL firebase.tools | bash
 firebase appdistribution:distribute "build/app/outputs/apk/release/app-release.apk" --app 1:239216846304:android:5e7b91e0de1dbbba583785 --groups android-testers --token $FIREBASE_CLI_TOKEN
